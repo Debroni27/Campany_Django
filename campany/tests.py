8491c615802406d3edf82d6247c2df984118fead
ikibirev <ikibirev.bb-n.ru>
2022-11-07 15:48:19 +0700

    - Добавил простой тест на статус index страницы

--- campany/tests.py
+++ campany/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class ViewsTestCase(TestCase):
+    def test_index_loads_properly(self):
+        response = self.client.get('https://127.0.0.1:8000')
+        self.assertEqual(response.status_code, 200)
